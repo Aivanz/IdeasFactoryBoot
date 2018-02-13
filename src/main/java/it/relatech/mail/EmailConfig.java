02f74f6f82acc7de37c23fc45b99ead83a958dcc
Ciro <ciro.dalessandro@outlook.it>
2018-02-13 16:38:27 +0100

    Revert "Ciro Mail"
    This reverts commit 1c9a5000684ed649a5865e5c31e8466e7695aa97.

--- src/main/java/it/relatech/mail/EmailConfig.java
+++
@@ -1,8 +0,0 @@
-package it.relatech.mail;
-
-public interface EmailConfig {
-
-	void sendSimpleMessage(String to, String subject, String text);
-
-	MailObject sendSimpleMessage(MailObject mailObject);
-}
